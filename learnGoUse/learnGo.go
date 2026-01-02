0155cdaf998b08ebe5274181a79f479bdb7e845b
edward sun <edwardsun007@gmail.com>
2026-01-01 22:44:14 -0800

    move some code

---
+++ learnGoUse/learnGo.go
@@ -0,0 +1,49 @@
+package learnGoUse
+
+import "fmt"
+
+type Employee struct {
+	Name string
+	Age  int
+}
+
+func (e Employee) displayInfo() {
+	fmt.Printf("Name: %s, Age: %d\n", e.Name, e.Age)
+}
+
+func PublicFunc() string {
+	return "PublicFunc"
+}
+
+func privateFunc() string {
+	return "privateFunc"
+}
+
+func LearnGoUse() {
+
+	e := Employee{
+		Name: "John",
+		Age:  30,
+	}
+	e.displayInfo()
+
+	pubMsg := PublicFunc()
+	fmt.Println(pubMsg)
+
+	privateMsg := privateFunc()
+	fmt.Println(privateMsg)
+
+	// fmt.Println("Hello world");
+	// age := 27;
+	// fmt.Println(age);
+
+	// var cities = [...]string {"Seattle", "San Jose"};
+	// fmt.Println(cities[0]);
+	// fmt.Println(cities[1]);
+
+	// var userNames = [...]string{"jinzhu1", "jinzhu2",  "jinzhu3"};
+	// for _, name := range userNames {
+	// 	fmt.Println(name);
+	// }
+
+}
